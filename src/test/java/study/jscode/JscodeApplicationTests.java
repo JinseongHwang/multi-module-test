1f3356e4f52a928852118f24e6ebe935e20e40ba
GYUTAEKIM <kkt219a@gmail.com>
2022-01-26 22:10:19 +0900

    feat: project init

---
+++ src/test/java/study/jscode/JscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
