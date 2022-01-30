39c8b24658f010d03829eb7ed7a5f30fc913cd8f
GYUTAEKIM <kkt219a@gmail.com>
2022-01-30 14:52:38 +0900

    fix: gradle 의존성 경고 이슈 수정

---
+++ jscode-api/src/main/java/study/jscode/JscodeApplication.java
@@ -0,0 +1,13 @@
+package study.jscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JscodeApplication.class, args);
+	}
+
+}
---
+++ jscode-api/src/test/java/study/jscode/JscodeApplicationTests.java
@@ -0,0 +1,7 @@
+package study.jscode;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JscodeApplicationTests {
+}
